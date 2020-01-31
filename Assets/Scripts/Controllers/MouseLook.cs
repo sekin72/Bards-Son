914643abc9583f280908dd2768fa5ae8d66bf75d
Şekip Kaan EKİN <kaan.ekin@ug.bilkent.edu.tr>
2020-02-01 02:56:26 +0300

    Player movement and camera movement
    P

---
+++ Assets/Scripts/Controllers/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform PlayerBody;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
